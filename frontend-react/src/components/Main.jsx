a3ca6deb0f954ad6e103c9eb51433252853a34e8
Gowtham <gowthamdevadiga2002@gmail.com>
2025-08-03 21:28:06 +0530

    homepage react

---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+    <>
+        <div className='container'>
+            <div className='p-5 text-center bg-light-dark'>
+                <h1 className='text-light'>TeamSuggest</h1>
+                <p className='text-light lead'>This one of the secure and advance team management system. Can be used for company, communities, etc</p>
+                <Button text="Login now" class="btn-outline-info"/>
+            </div>
+        </div>
+    
+    </>
+  )
+}
+
+export default Main
