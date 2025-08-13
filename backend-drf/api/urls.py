3dc3c371f1af5c17ecc4e7f57469ff3ebaabab02
Gowtham <gowthamdevadiga2002@gmail.com>
2025-08-14 01:03:43 +0530

    prediction visulation

--- backend-drf/api/urls.py
+++ backend-drf/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from accounts import views as UserViews
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import StockPredictionAPIView
 
 urlpatterns = [
     path('register/',UserViews.RegisterView.as_view()),
@@ -8,4 +9,7 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('protected-view/', UserViews.ProtectedView.as_view()),
+
+    #Path for stock prediction API
+    path('predict/', StockPredictionAPIView.as_view(), name='stock_prediction_api'),
 ]
